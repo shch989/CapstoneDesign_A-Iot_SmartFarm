f4f1db2488c722230775fe46f361f571f83d3995
shch989 <tankpop11@gmail.com>
2023-05-07 23:56:08 +0900

    footer 코드 클린화

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -4,9 +4,10 @@ import styled from 'styled-components'
 const Container = styled.footer`
   width: 100%;
   background-color: #e0ffff;
-  padding-top: 1rem;
+  padding: 0.7rem 0;
   bottom: 0;
 `
+
 const MainFooter = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -33,11 +34,13 @@ const Section = styled.div`
     font-weight: bold;
     color: #222;
     margin: 0;
+    margin-bottom: 5px;
   }
 
   p {
     font-size: 0.8rem;
     line-height: 1.5;
+    margin: 0;
 
     b {
       font-weight: bold;
@@ -55,57 +58,99 @@ const Link = styled.a`
   }
 `
 
+const teamMembers = [
+  {
+    sectionTitle: '프로젝트 소개',
+    sectionContent: [
+      {
+        title: '팀명',
+        content: '건강하게만 자라다오',
+      },
+      {
+        title: '지도교수',
+        content: '황이철 교수(님)',
+      },
+      {
+        title: '팀장',
+        content: '전채욱',
+      },
+      {
+        title: '팀원',
+        content: '조성현, 오두환, 박정혁, 이승우',
+      },
+    ],
+  },
+  {
+    sectionTitle: '사용 문의',
+    sectionContent: [
+      {
+        title: 'Gmail',
+        content: (
+          <Link href="mailto:Tankpop11@gmail.com">Tankpop11@gmail.com</Link>
+        ),
+      },
+      {
+        title: 'Tel',
+        content: '010-9976-2790',
+      },
+      {
+        title: 'A.I Github',
+        content: (
+          <Link href="https://github.com/ETTE154">
+            https://github.com/ETTE154
+          </Link>
+        ),
+      },
+      {
+        title: 'S/W Github',
+        content: (
+          <Link href="https://github.com/shch989">
+            https://github.com/shch989
+          </Link>
+        ),
+      },
+    ],
+  },
+  {
+    sectionTitle: '팀원 역할분담',
+    sectionContent: [
+      {
+        title: 'A.I 담당자',
+        content: '전채욱',
+      },
+      {
+        title: 'S/W 담당자',
+        content: '조성현',
+      },
+      {
+        title: 'H/W 담당자',
+        content: '오두환',
+      },
+      {
+        title: 'H/W 팀원',
+        content: '박정혁, 이승우',
+      },
+    ],
+  },
+]
+
 const Footer = () => {
   return (
     <Container>
       <MainFooter>
-        <Section>
-          <h4>프로젝트 소개</h4>
-          <p>
-            <b>팀명 : 건강하게만 자라다오</b>
-            <br />
-            <b>지도교수 :</b> 황이철 교수(님)
-            <br />
-            <b>팀장 :</b> 전채욱
-            <br />
-            <b>팀원 :</b> 조성현, 오두환, 박정혁, 이승우
-          </p>
-        </Section>
-        <Section>
-          <h4>사용 문의</h4>
-          <p>
-            <b>Gmail :</b>{' '}
-            <Link href="mailto:Tankpop11@gmail.com">Tankpop11@gmail.com</Link>
-            <br />
-            <b>Tel :</b> 010-9976-2790
-            <br />
-            <b>A.I Github :</b>{' '}
-            <Link href="https://github.com/ETTE154">
-              https://github.com/ETTE154
-            </Link>
-            <br />
-            <b>S/W Github :</b>{' '}
-            <Link href="https://github.com/shch989">
-              https://github.com/shch989
-            </Link>
-          </p>
-        </Section>
-        <Section>
-          <h4>팀원 역할분담</h4>
-          <p>
-            <b>A.I 담당자 :</b> 전채욱
-            <br />
-            <b>S/W 담당자 :</b> 조성현
-            <br />
-            <b>H/W 담당자 :</b> 오두환
-            <br />
-            <b>H/W 팀원 :</b> 박정혁, 이승우
-            <br />
-          </p>
-        </Section>
+        {teamMembers.map((member, index) => (
+          <Section key={index}>
+            <h4>{member.sectionTitle}</h4>
+            {member.sectionContent.map((content, index) => (
+              <p key={index}>
+                <b>{content.title}: </b>
+                {content.content}
+              </p>
+            ))}
+          </Section>
+        ))}
       </MainFooter>
     </Container>
   )
 }
-
 export default Footer
