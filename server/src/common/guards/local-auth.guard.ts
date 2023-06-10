3170e29269ac39cdcc6436ad1dea2828649ba8b0
shch989 <tankpop11@gmail.com>
2023-06-10 23:58:45 +0900

    guard 폴더 추가

---
+++ server/src/common/guards/local-auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'email' }); // 이메일을 사용하여 인증할 것이므로, usernameField를 'email'로 설정합니다.
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    return user;
+  }
+}
