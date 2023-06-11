91b8676dbead269b742247e171e8b02b24d00972
shch989 <tankpop11@gmail.com>
2023-06-11 21:52:32 +0900

    dht기능 로그아웃시에 socket 서버 끊기

--- server/src/dht/dht.gateway.ts
+++ server/src/dht/dht.gateway.ts
@@ -1,17 +1,18 @@
-import { OnGatewayConnection, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { DhtService } from './dht.service';
 import { DataDto } from 'src/users/dtos/data.dto';
 import { JwtService } from '@nestjs/jwt';
 
 @WebSocketGateway(5000, { namespace: 'dht', cors: { origin: '*' } })
-export class DhtGateway implements OnGatewayConnection {
-  private interval: NodeJS.Timeout | undefined;
+export class DhtGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  private intervalMap: Map<string, NodeJS.Timeout> = new Map();
 
   constructor(
     private readonly dhtService: DhtService,
     private readonly jwtService: JwtService,
-  ) { }
+  ) {}
 
   @WebSocketServer()
   server: Server;
@@ -32,17 +33,33 @@ export class DhtGateway implements OnGatewayConnection {
       const userId = decodedToken.sub; // 토큰에서 유저 ID 추출
       // 유효한 토큰인 경우 연결 허용
       socket.join(userId); // 유저별로 개별 방에 조인
-      if (!this.interval) {
-        this.accessData(userId);
-        this.interval = setInterval(() => {
-          this.updateData(userId);
-        }, 60000);
-      }
+      this.startUpdateData(userId);
     } catch (err) {
       // 토큰이 유효하지 않을 경우 연결 거부
       console.log('유효하지 않는 토큰입니다.');
       socket.disconnect();
     }
+  }
+
+  handleDisconnect(socket: Socket) {
+    const userId = this.getUserIdFromSocket(socket);
+    if (userId && this.intervalMap.has(userId)) {
+      clearInterval(this.intervalMap.get(userId));
+      this.intervalMap.delete(userId);
+    }
+  }
+
+  private getUserIdFromSocket(socket: Socket): string | undefined {
+    // 소켓에서 유저 ID를 추출하는 로직을 구현해야 합니다.
+    // 예시로서는 토큰에서 유저 ID를 추출하는 것으로 가정합니다.
+    const token = socket.handshake.auth.token; 
+    if (token) {
+      const decodedToken = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET_KEY,
+      });
+      return decodedToken.sub;
+    }
+    return undefined;
   }
 
   private async accessData(userId: string) {
@@ -51,6 +68,16 @@ export class DhtGateway implements OnGatewayConnection {
       return this.emitData(userId, sensorData);
     } catch (err) {
       console.error(err);
+    }
+  }
+
+  private async startUpdateData(userId: string) {
+    if (!this.intervalMap.has(userId)) {
+      const interval = setInterval(() => {
+        this.updateData(userId);
+      }, 60000);
+      this.intervalMap.set(userId, interval);
+      this.updateData(userId); // 첫 번째 호출을 추가합니다.
     }
   }
 
@@ -92,10 +119,10 @@ export class DhtGateway implements OnGatewayConnection {
   private emitHumidityData(userId: string, humidity: number[]) {
     this.server.to(userId).emit('humidityData', humidity);
     console.log('humidity', humidity);
-  }
+  } 
 
   private emitData(userId: string, userData: DataDto) {
     this.emitTemperatureData(userId, userData.sensor.temperature);
     this.emitHumidityData(userId, userData.sensor.humidity);
   }
-}
+} 
